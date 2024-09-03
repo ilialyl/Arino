c440ccbb62d5eafd804674f32fbb59b23c056452
lyns0 <cyanallybusiness@gmail.com>
2024-09-03 12:11:33 +0700

    Refactor: make names in query_commands.rs more descriptive

--- src/database/query_commands.rs
+++ src/database/query_commands.rs
@@ -2,12 +2,12 @@ use rusqlite::{Connection, Result};
 #[allow(dead_code)]
 
 pub fn query_all_dishes(conn: Connection) -> Result<()> {
-    let mut statement = conn.prepare("Select id, name FROM dishes")?;
-    let dish_iter = statement.query_map([], |row| {
+    let mut dishes_query = conn.prepare("Select id, name FROM dishes")?;
+    let dishes_iter = dishes_query.query_map([], |row| {
         Ok((row.get::<_, i32>(0)?, row.get::<_, String>(1)?))
     })?;
 
-    for dish in dish_iter {
+    for dish in dishes_iter {
         let (id, name) = dish?;
         println!("ID {id}: {name}");
     }
@@ -16,32 +16,32 @@ pub fn query_all_dishes(conn: Connection) -> Result<()> {
 }
 
 pub fn query_recipe(dish_name: &str, conn: &Connection) -> Result<()> {
-    let mut ingredients: Vec<String> = Vec::new();
+    let mut ingredient_list: Vec<String> = Vec::new();
 
-    let mut get_dish_id = conn.prepare("SELECT id FROM dishes WHERE name = ?1;")?;
-    let dish_id: u32 = get_dish_id.query_row([dish_name], |row| {
+    let mut dish_id_query = conn.prepare("SELECT id FROM dishes WHERE name = ?1;")?;
+    let dish_id: u32 = dish_id_query.query_row([dish_name], |row| {
         row.get(0)
     })?;
     
-    let mut get_ingredient_id = conn.prepare("SELECT ingredient_id FROM recipes WHERE dish_id = ?1;")?;
-    let ingredient_id_iter = get_ingredient_id.query_map([dish_id], |row| {
+    let mut ingredient_id_query = conn.prepare("SELECT ingredient_id FROM recipes WHERE dish_id = ?1;")?;
+    let ingredient_id_iter = ingredient_id_query.query_map([dish_id], |row| {
         Ok(row.get::<_, i32>(0)?)
     })?;
 
     for id in ingredient_id_iter {
-        let mut get_ingredient_name = conn.prepare("SELECT name FROM ingredients WHERE id = ?1;")?;
-        let ingredient_name_iter = get_ingredient_name.query_map([id?], |row| {
+        let mut ingredient_names_query = conn.prepare("SELECT name FROM ingredients WHERE id = ?1;")?;
+        let ingredient_names_iter = ingredient_names_query.query_map([id?], |row| {
             Ok(row.get::<_, String>(0)?)
         })?;
 
-        for name in ingredient_name_iter {
-            ingredients.push(name?);
+        for ingredient_name in ingredient_names_iter {
+            ingredient_list.push(ingredient_name?);
         }
     }
 
     println!("Recipe for {dish_name}:");
-    for name in ingredients {
-        println!("- {name}");
+    for ingredient_name in ingredient_list {
+        println!("- {ingredient_name}");
     }
 
     Ok(())
