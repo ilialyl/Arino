e4c0df5b4f784cb858390d2767657fd04230ef0e
lyns0 <cyanallybusiness@gmail.com>
2024-09-14 11:11:51 +0700

    Fix: fix delete::ingredient_from_recipe()

--- src/database/delete.rs
+++ src/database/delete.rs
@@ -64,6 +64,7 @@ pub async fn ingredient_from_recipe() -> Result<()> {
     println!("Are you sure you want to delete this ingredient from this recipe?");
     if prompt("[Y/N]") != "y" {
         println!("Deletion aborted");
+        return Ok(());
     }
     
     let mut stmt = conn.prepare("DELETE FROM recipes WHERE dish_id = ?1 AND ingredient_id = ?2;")?;
@@ -80,6 +81,57 @@ pub async fn ingredient_from_recipe() -> Result<()> {
     Ok(())
 }
 
-pub fn dish() {
+pub async fn dish() -> Result<()> {
+    if !has_internet_access().await {
+        return Ok(());
+    }
+    
+    match fetch(Database::Main).await {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("{e}");
+            return Ok(());
+        },
+    }
 
+    let conn = get_connection();
+
+    let dish_id = loop {
+        let dish_name = prompt("Dish name");
+        if dish_name.is_empty() {
+            return Ok(());
+        }
+
+        let retrieved_dish_id: u32 = match conn.query_row("SELECT id FROM dishes WHERE name = ?1;", [&dish_name], |row| row.get(0)) {
+            Ok(id) => id,
+            Err(rusqlite::Error::QueryReturnedNoRows) => {
+                eprintln!("Invalid dish name");
+                continue;
+            },
+            Err(e) => {
+                eprintln!("Error: {e}");
+                continue;
+            }
+        };
+        break retrieved_dish_id;
+    };
+
+    println!("Are you sure you want to delete this dish along with its recipe?");
+    if prompt("[Y/N]") != "y" {
+        println!("Deletion aborted");
+        return Ok(());
+    }
+
+    let mut stmt = conn.prepare("DELETE FROM recipes WHERE dish_id = ?1 AND ingredient_id = ?2;")?;
+    stmt.execute((&dish_id, &ingredient_id))?;
+
+    match sync().await {
+        Ok(_) => {},
+        Err(e) => {
+            eprintln!("{e}");
+            return Ok(());
+        },
+    }
+
+    Ok(())
 }
