a15c5899329a4f47e581ee90d7a5192f8683c7dd
lyns0 <lynzies.sl@gmail.com>
2025-06-10 19:01:22 +0700

    Add comments in helper.rs

--- src/helper.rs
+++ src/helper.rs
@@ -1,9 +1,11 @@
 use std::io;
 
+// Flushes the IO stream
 pub fn flush() {
     io::Write::flush(&mut io::stdout()).expect("flush failed!");
 }
 
+// Gets the mean of floating point values in a vector.
 pub fn calculate_mean(float_vec: Vec<f32>) -> f32{
     let count = float_vec.len() as f32;
     let sum = float_vec.iter().fold(0.0, |acc, value| acc + value);
