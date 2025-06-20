09b371d3d19b540b2e0af61a1411a4eb70ca64fb
lyns0 <lynzies.sl@gmail.com>
2025-06-20 12:36:54 +0700

    Make main handle credential-less use of the app

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,27 @@ pub mod miscellaneous;
 use clap::Parser;
 use cli::commands::Cli;
 use database::{
-    cloud::{Database, has_internet_access},
+    cloud::{Database, get_credentials, has_internet_access},
     database_exists,
 };
+
 use tokio;
+
+use crate::database::cloud::download_database;
 
 #[tokio::main]
 async fn main() {
-    if !database_exists() {
-        if has_internet_access().await {
+    if !database_exists() & has_internet_access().await {
+        if get_credentials().is_err() {
+            download_database();
+        } else {
             match database::cloud::fetch(Database::Main).await {
                 Ok(_) => {}
                 Err(e) => println!("{e}"),
             }
-        } else {
-            eprintln!("Internet access is required to fetch database for first use!");
         }
+    } else {
+        eprintln!("Internet access is required to fetch database for first use!");
     }
 
     let cli = Cli::parse();
