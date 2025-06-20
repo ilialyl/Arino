4dbcc2d37a3f5121075362a2a81911c9567adb17
lyns0 <lynzies.sl@gmail.com>
2025-06-20 11:06:34 +0700

    Clean up unnecessary functions in cli_operations

--- src/cli_operations/mod.rs
+++ src/cli_operations/mod.rs
@@ -1,10 +1,2 @@
 pub mod commands;
 pub mod user_input;
-
-use crate::miscellaneous::flush;
-
-// Flushes the input stream and prints cancel message
-pub fn cancel_prompt() {
-    flush();
-    print!("\rAction Canceled!");
-}
--- src/cli_operations/user_input.rs
+++ src/cli_operations/user_input.rs
@@ -1,14 +1,6 @@
 use crate::miscellaneous::flush;
 use std::io::stdin;
 
-// Splits a string to a vector depending on what the separator is.
-pub fn split_to_vec(separator: &str, user_input: String) -> Vec<String> {
-    let split_iter = user_input.split(separator);
-    let separated_inputs_vec: Vec<String> = split_iter.map(|s| s.trim().to_string()).collect();
-
-    separated_inputs_vec
-}
-
 // Prompts user to input something based on the function argument and return the user input.
 pub fn prompt(prompt: &str) -> String {
     let mut user_input = String::new();
